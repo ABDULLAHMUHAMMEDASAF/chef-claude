0e781899a956bbe075a4eeb07f0c991b1b9e0d03
ABDULLAHMUHAMMEDASAF <emrecanemre49@gmail.com>
2026-04-23 17:19:56 +0300

    It was updated on 23.04.2026 at 17.19

---
+++ src/components/Main.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./Main.css";
+
+function Main() {
+  const [ingredients, setIngredients] = useState([
+    "Chicken",
+    "Oregano",
+    "Tomatoes",
+  ]);
+
+  const ingredientsListItems = ingredients.map((item) => (
+    <li key={item}>{item}</li>
+  ));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prev) => [...prev, newIngredient]);
+    e.currentTarget.reset();
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
+        <input
+          placeholder="e.g. oregano"
+          type="text"
+          aria-label="add-ingredient-form"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>{ingredientsListItems}</ul>
+    </main>
+  );
+}
+export default Main;
