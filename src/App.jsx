af20a5f066d7e1597b7afe10fd975d1227d52fb5
ABDULLAHMUHAMMEDASAF <emrecanemre49@gmail.com>
2026-05-07 20:25:30 +0300

    It was update d

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 
-//! TEST COMPONENT
-import Test from "./components/Test.jsx";
-
 function App() {
   return (
     <>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,27 @@
-import { useState } from "react";
+import React from "react";
 import "./Main.css";
 
 function Main() {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = React.useState([]);
+  const [inputValue, setInputValue] = React.useState("");
 
-  const ingredientsListItems = ingredients.map((item) => (
-    <li key={item}>{item}</li>
+  const ingredientsListItems = ingredients.map((item, index) => (
+    <li key={index}>{item}</li>
   ));
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const newIngredient = formData.get("ingredient");
-    setIngredients((prev) => [...prev, newIngredient]);
-    e.currentTarget.reset();
+  function formHandle() {
+    setIngredients((prev) => [...prev, inputValue]);
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={formHandle} className="add-ingredient-form">
         <input
           placeholder="e.g. oregano"
           type="text"
           aria-label="add-ingredient-form"
           name="ingredient"
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button>Add ingredient</button>
       </form>
@@ -31,4 +29,5 @@ function Main() {
     </main>
   );
 }
+
 export default Main;
